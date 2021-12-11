76029579adfe592578fba3af5f1052b4e91470f8
Vlad Shutsko <v.5hutko@yandex.by>
2021-12-12 01:40:53 +0300

    all done

---
+++ script.js
@@ -0,0 +1,38 @@
+const popupOpenButton = document.querySelector('.user__edit');
+const popupCloseButton = document.querySelector('.popup__close');
+
+const popup = document.querySelector('.popup');
+
+let userName = document.querySelector('.user__text_name');
+let userProfession = document.querySelector('.user__text_profession');
+
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__profession');
+
+
+function togglePopoup() {
+    popup.classList.toggle('popup_opened');
+}
+popupOpenButton.addEventListener('click', togglePopoup);
+popupCloseButton.addEventListener('click', togglePopoup);
+
+
+
+function setDefaultPopupValue() {
+
+
+    nameInput.setAttribute('value', userName.textContent);
+    jobInput.setAttribute('value', userProfession.textContent);
+
+}
+
+popupOpenButton.addEventListener('click', setDefaultPopupValue);
+
+let saveButton = document.querySelector(".popup__save");
+
+function saveName() {
+    userName.textContent = nameInput.value;
+    userProfession.textContent = jobInput.value;
+    togglePopoup();
+}
+saveButton.addEventListener('click', saveName);
